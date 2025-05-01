5d7466aebe82fac8de8f41ed406b77a4842b0038
Guilherme Novais <guilhermens@ufmg.com>
2025-05-01 08:43:03 -0300

    feat: show financing calculator on main page

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,3 +1,19 @@
-<template></template>
+<template>
+  <h1>Simulador de Financiamento</h1>
+  <FinancingForm @update="handleInput" />
+  <FinancingResult :result="result" />
+</template>
 
-<script lang="ts" setup></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import FinancingForm from "@/components/FinancingForm.vue";
+import FinancingResult from "@/components/FinancingResult.vue";
+import type { FinancingData } from "@/types/FinancingData";
+import { calculateFinancing } from "@/utils/calculateFinancing";
+
+const result = ref<ReturnType<typeof calculateFinancing>>(null);
+
+function handleInput(data: FinancingData) {
+  result.value = calculateFinancing(data);
+}
+</script>
