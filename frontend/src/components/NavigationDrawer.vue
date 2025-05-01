a6736914383eedafb4598a3ef83f553ce11e13ff
Guilherme Novais <guilhermens@ufmg.com>
2025-05-01 07:29:50 -0300

    chore: create navigation drawer component

---
+++ frontend/src/components/NavigationDrawer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-navigation-drawer v-model="drawer" temporary>
+    <v-list dense nav>
+      <v-list-item
+        v-for="item in items"
+        :title="item.title"
+        :prepend-icon="item.icon"
+        :key="item.title"
+        :to="item.to"
+        link
+      >
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+const drawer = ref(true);
+
+const items = [
+  { title: "Calculadora", icon: "mdi-calculator", to: "/" },
+  { title: "Simulações Salvas", icon: "mdi-archive", to: "/salvas" },
+];
+</script>
