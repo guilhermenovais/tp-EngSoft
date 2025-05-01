371ab7701a399bdc6ffb9901cee23dacefbefe54
Guilherme Novais <guilhermens@ufmg.com>
2025-05-01 07:30:01 -0300

    chore: create header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-app-bar-nav-icon @click="$emit('toggle-drawer')" />
+
+    <v-app-bar-title>Calculadora de Financiamentos</v-app-bar-title>
+  </v-app-bar>
+</template>
