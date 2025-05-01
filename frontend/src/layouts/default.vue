42347d9a5feb0e0b69b7da41515a408e902fb9f5
Guilherme Novais <guilhermens@ufmg.com>
2025-05-01 07:30:48 -0300

    feat: add default layout with header and nav drawer

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -1,8 +1,14 @@
 <template>
   <v-main>
+    <Header @toggle-drawer="drawer = !drawer" />
+    <NavigationDrawer v-model="drawer" />
     <router-view />
   </v-main>
 </template>
 
 <script lang="ts" setup>
+import Header from "@/components/Header.vue";
+import NavigationDrawer from "@/components/NavigationDrawer.vue";
+
+const drawer = ref(false);
 </script>
