19a9b67189eea31bc29ccb6264c2aa8fbc93c418
Guilherme Novais <guilhermenovaisdesouza@proton.me>
2025-05-04 09:26:24 -0300

    fix: correct api base url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,10 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: "/backend/",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 export default api;
+
